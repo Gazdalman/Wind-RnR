0a42c57fd426a13a7e580782049e57ff84fcbb41
Toney <ta.winston@outlook.com>
2023-09-28 16:57:14 -0500

    Added User migration and Seeds

---
+++ backend/db/seeders/20230928204603-demo-user.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await User.bulkCreate([
+    {
+      username: 'FutRWizKing',
+      email: 'asta@user.io',
+      hashedPassword: bcrypt.hashSync('password'),
+    },
+    {
+      username: 'theRLFutRWK',
+      email: 'yuno@user.io',
+      hashedPassword: bcrypt.hashSync('password2'),
+    },
+    {
+      username: 'ShadwCap',
+      email: 'yami@user.io',
+      hashedPassword: bcrypt.hashSync('password3'),
+    },
+   ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['FutRWizKing','theRLFutRWK','ShadwCap'] }
+    }, {});
+  }
+};
