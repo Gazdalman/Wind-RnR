6dc38b7c5da0aaea41c294260aa113f79cefeb70
Toney <ta.winston@outlook.com>
2023-09-28 14:51:38 -0500

    Done with first day of auth-me

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,4 +4,12 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
+router.get('/csrf/restore', (req,res) => {
+  const token = req.csrfToken();
+  res.cookie('XSRF-TOKEN', token);
+  res.status(200).json({
+    'XSRF-Token': token
+  });
+});
+
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,13 +5,7 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/api/csrf/restore', (req,res) => {
-  const token = req.csrfToken();
-  res.cookie('XSRF-TOKEN', token);
-  res.status(200).json({
-    'XSRF-Token': token
-  });
-});
+
 
 
 module.exports = router;
