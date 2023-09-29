e996e6522db0a2fb21b0eaaf5b2cc6150b7ca35c
Toney <ta.winston@outlook.com>
2023-09-29 11:50:40 -0500

    Add User login route handler

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../../db/models')
-
-const { setTokenCookie, restoreUser } = require('../../utils/auth.js');
+const usersRouter  = require('./users.js');
+const sessionRouter  = require('./session.js')
+const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 router.get('/csrf/restore', (req,res) => {
   const token = req.csrfToken();
