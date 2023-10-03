a1472bdc9a9a6b7db8f39bfc50c6b42a59d34ee8
Toney <ta.winston@outlook.com>
2023-10-03 18:36:29 -0500

    Try again

--- backend/db/migrations/20231002214502-create-review-image.js
+++ backend/db/migrations/20231002214502-create-review-image.js
@@ -1,10 +1,57 @@
+// 'use strict';
+// /** @type {import('sequelize-cli').Migration} */
+
+// let options = {};
+
+// if (process.env.NODE_ENV === "production") {
+//   options.schema = process.env.SCHEMA;
+// }
+
+// module.exports = {
+//   async up(queryInterface, Sequelize) {
+//     await queryInterface.createTable('ReviewImages', {
+//       id: {
+//         allowNull: false,
+//         autoIncrement: true,
+//         primaryKey: true,
+//         type: Sequelize.INTEGER
+//       },
+//       reviewId: {
+//         type: Sequelize.INTEGER,
+//         references: {
+//           model: "Reviews",
+//           key: 'id'
+//         },
+//         onDelete: 'CASCADE'
+//       },
+//       url: {
+//         type: Sequelize.STRING,
+//         allowNull: false
+//       },
+//       createdAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE,
+//         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+//       },
+//       updatedAt: {
+//         allowNull: false,
+//         type: Sequelize.DATE,
+//         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+//       }
+//     }, options);
+//   },
+//   async down(queryInterface, Sequelize) {
+//     options.tableName = "ReviewImages"
+//     await queryInterface.dropTable(options, {});
+//   }
+// };
+
+
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
-
 let options = {};
-
-if (process.env.NODE_ENV === "production") {
-  options.schema = process.env.SCHEMA;
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
 }
 
 module.exports = {
@@ -19,7 +66,7 @@ module.exports = {
       reviewId: {
         type: Sequelize.INTEGER,
         references: {
-          model: "Reviews",
+          model: 'Reviews',
           key: 'id'
         },
         onDelete: 'CASCADE'
@@ -31,17 +78,17 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
       }
     }, options);
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "ReviewImages"
-    await queryInterface.dropTable(options, {});
+    await queryInterface.dropTable(options);
   }
 };
