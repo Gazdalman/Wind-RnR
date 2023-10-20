ced9c76f06d7642b264c391c5d5e1c1a68a50473
Toney <ta.winston@outlook.com>
2023-10-20 08:50:27 -0500

    Finished login form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from "./components/LoginFormPage/index.js";
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div>
+      <Switch>
+        <Route exact path='/'>
+          <h1>Hello from App</h1>
+        </Route>
+        <Route path='/login'>
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </div>
+
   );
 }
 
