f44afc983b426e922827a15384f8ac8c58c7f323
Toney <ta.winston@outlook.com>
2024-01-20 13:48:57 -0600

    namanad

--- backend/db/migrations/20240116223921-create-like.js
+++ backend/db/migrations/20240116223921-create-like.js
@@ -3,8 +3,8 @@
 
 let options = {};
 
-if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;  // define your schema in options object
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
 }
 
 module.exports = {
