5797f41dc91500f3a5aede8411f315201c6999a8
Toney <ta.winston@outlook.com>
2024-02-23 16:20:55 -0600

    Fixed the likes model

--- backend/db/migrations/20240116223921-create-like.js
+++ backend/db/migrations/20240116223921-create-like.js
@@ -20,14 +20,16 @@ module.exports = {
         allowNull:false,
         references: {
           model: "Users"
-        }
+        },
+        onDelete: 'CASCADE'
       },
       spotId: {
         type: Sequelize.INTEGER,
         allowNull:false,
         references: {
           model: "Spots"
-        }
+        },
+        onDelete: 'CASCADE'
       },
       createdAt: {
         allowNull: false,
